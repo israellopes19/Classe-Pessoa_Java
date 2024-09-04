2b6126fe07cd39959554c164b43f9c2bf217c628
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-09-03 23:03:43 -0300

    Delete src directory

--- src/Caminhao.java
+++
@@ -1,8 +0,0 @@
-public class Caminhao extends Veiculo{
-    public Caminhao(String marca, String modelo, double preco) {
-        super(marca, modelo, preco);
-    }
-
-    public Caminhao() {
-    }
-}
