dcbaf7d6ee3e6b5557abc6ed2685fab59d214e91
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-09-03 23:04:00 -0300

    Add files via upload

---
+++ Veiculo_Class_java/src/Caminhao.java
@@ -0,0 +1,8 @@
+public class Caminhao extends Veiculo{
+    public Caminhao(String marca, String modelo, double preco) {
+        super(marca, modelo, preco);
+    }
+
+    public Caminhao() {
+    }
+}
---
+++ Veiculo_Class_java/src/Moto.java
@@ -0,0 +1,8 @@
+public class Moto extends Veiculo {
+    public Moto(String marca, String modelo, double preco) {
+        super(marca, modelo, preco);
+    }
+
+    public Moto() {
+    }
+}
